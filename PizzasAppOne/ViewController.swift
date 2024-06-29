7de4850b47b30b302806f8ad9e360380de19ce3e
Angel Lopez <a.o.lopez.cruz@icloud.com>
2024-06-29 01:06:14 -0600

    Initial Commit

---
+++ PizzasAppOne/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PizzasAppOne
+//
+//  Created by Angel Octavio Lopez Cruz on 29/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
